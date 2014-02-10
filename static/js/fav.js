897d27fca761b936bab34cb04629a79ee424059c
fuyao <fuyao@douban.com>
2014-02-10 15:58:12 +0800

    bugfix

--- static/js/fav.js
+++ static/js/fav.js
@@ -4,7 +4,7 @@ function do_fav(entry_id, obj) {
   $.post("/j/do_fav",{photo_id:entry_id}, function(r) {
     if (r.code === 200) {
       $(obj).removeClass().addClass("Button Button11 WhiteButton disabled clickable unlike_pin");
-      $(obj).html("<strong>Unlike</strong><span></span>");i
+      $(obj).html("<strong>Unlike</strong><span></span>");
     }
   });
 }
