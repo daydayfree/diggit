97b49eee0b28a1823109b687ec284c0987064103
xueming <xueming@douban.com>
2014-02-12 00:48:19 +0800

    Use Pillow replace PIL

--- view/j/like.py
+++ view/j/like.py
@@ -7,15 +7,18 @@ from view import BaseHandler
 class LikeHandler(BaseHandler):
 
     def post(self):
-        photo_id = self.get_argument('photo_id', '')
-        photo = Photo.get(photo_id)
-        photo.inc_like_count()
         self.set_header("Content-Type", "application/json")
-        r = {
-            'code': 200,
-            'like_count': photo.like_count,
-            'photo_id': photo_id
-        }
+        r = {'code': 500}
+        photo_id = self.get_argument('photo_id')
+        if photo_id:
+            photo = Photo.get(photo_id)
+            if photo:
+                photo.inc_like_count()
+                r = {
+                    'code': 200,
+                    'like_count': photo.like_count,
+                    'photo_id': photo_id
+                }
         self.write(r)
         self.finish()
 
